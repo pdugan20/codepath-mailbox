1c69caebc8746215ff718659fed5ffb3f22d53ca
Patrick Dugan <dugan.pat@gmail.com>
2015-05-16 19:23:23 -0700

    initial commit
    initial view controller and skeleton readme

---
+++ codepath-mailbox/MailboxViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MailboxViewController.swift
+//  codepath-mailbox
+//
+//  Created by Patrick Dugan on 5/16/15.
+//  Copyright (c) 2015 dailydoog. All rights reserved.
+//
+
+import UIKit
+
+class MailboxViewController: UIViewController {
+
+    @IBOutlet var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentSize = CGSize(width: 320, height: 1429)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
