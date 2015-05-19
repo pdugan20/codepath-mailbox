a32ea25aa8b7c49cf0815ade5a815badc12a29ff
Patrick Dugan <patrick@nextdoor.com>
2015-05-18 17:50:23 -0700

    added pan to menu and tweaking timings
    added edge pan left and right gestures to open and close menus and
    adjusted animation timing

--- codepath-mailbox/MailboxViewController.swift
+++ codepath-mailbox/MailboxViewController.swift
@@ -14,13 +14,52 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var mainView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
     
-    let openMenuOffset = CGFloat(300)
+    var openMenuOffset = CGFloat(300)
+    var mainViewCenter = CGPoint()
+    var messageCenter = CGPoint()
+    var leftViewOrigin = CGPoint()
+    var rightViewOrigin = CGPoint()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         scrollView.contentSize = CGSize(width: 320, height: 2240)
         
+        self.mainView.frame.origin.x = 0
+        self.mainViewCenter = self.mainView.center
+        
+        var edgePanGestureLeft = UIScreenEdgePanGestureRecognizer(target: self, action: "onMenuSwipeOpen:")
+        edgePanGestureLeft.edges = UIRectEdge.Left
+        self.mainView.addGestureRecognizer(edgePanGestureLeft)
+        
+        var edgePanGestureRight = UIScreenEdgePanGestureRecognizer(target: self, action: "onMenuSwipeClose:")
+        edgePanGestureRight.edges = UIRectEdge.Right
+        self.mainView.addGestureRecognizer(edgePanGestureRight)
+        
+    }
+    
+    func onMenuSwipeOpen(gestureRecognizer: UIScreenEdgePanGestureRecognizer) {
+        var translation = gestureRecognizer.translationInView(view)
+        var velocity = gestureRecognizer.velocityInView(view)
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.Began {
+            
+        }
+        else if gestureRecognizer.state == UIGestureRecognizerState.Changed {
+            self.mainView.center.x = translation.x + mainViewCenter.x
+        }
+        else if gestureRecognizer.state == UIGestureRecognizerState.Ended {
+            if (velocity.x >= 0) {
+                openMenu()
+            }
+            else {
+                closeMenu()
+            }
+        }
+    }
+    
+    func onMenuSwipeClose(gestureRecognizer: UIScreenEdgePanGestureRecognizer) {
+        closeMenu()
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,14 +77,14 @@ class MailboxViewController: UIViewController {
     
     func openMenu() {
         self.scrollView.scrollEnabled = false
-        UIView.animateWithDuration(0.4, animations: {
+        UIView.animateWithDuration(0.6, animations: {
             self.mainView.frame.origin.x = self.openMenuOffset
         })
     }
     
     func closeMenu() {
         self.scrollView.scrollEnabled = true
-        UIView.animateWithDuration(0.4, animations: {
+        UIView.animateWithDuration(0.6, animations: {
             self.mainView.frame.origin.x = 0
         })
     }
